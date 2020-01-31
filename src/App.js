da3e1be05b272651149ec994b9e55d326ecc00ad
Varma Bhupatiraju <varmab@gmail.com>
2020-01-31 11:20:40 +0530

    todos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 import Welcome from './Welcome'
 import Counter from './Counter'
+import Friends from './Friends';
+import Todos from './Todos';
 
 class App extends Component{
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <Welcome title="Varma"/>
-          <Welcome title="Ram"/>
+          <Welcome title="Varma" year={2022} password="123456"/>
         </header>
-        <Counter/>
+        <Todos/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Welcome extends Component{
     constructor(){
@@ -16,9 +17,26 @@ class Welcome extends Component{
 
     render(){
         return(
-            <h1 onClick={this.updateClicks}>Welcome to { this.props.title } - ({this.state.clicks})</h1>
+            <h1 onClick={this.updateClicks}>Welcome to { this.props.title } - {this.props.year} - ({this.state.clicks})</h1>
         )
     }
+}
+
+Welcome.propTypes={
+    title:PropTypes.string.isRequired,
+    year:PropTypes.number,
+    password(props, propName, component){
+        if(!(propName in props)){
+          return new Error(`missing ${propName}`)
+        }
+        if(props[propName].length < 6){
+          return new Error(`${propName} was too short`)
+        }
+      }
+}
+
+Welcome.defaultProps={
+    year:2020
 }
 
 export default Welcome;
